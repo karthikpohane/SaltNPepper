aaf74133a27fe0e906a4fdd86e924b2cb54d6ca4
karthikpohane <karthik.pohane@mca.christuniversity.in>
2023-09-07 10:19:59 +0530

    FAB update

--- app/src/main/java/com/example/salt/likedSongs_pg.java
+++ app/src/main/java/com/example/salt/likedSongs_pg.java
@@ -3,6 +3,8 @@ package com.example.salt;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -17,6 +19,7 @@ import com.example.jean.jcplayer.model.JcAudio;
 import com.example.jean.jcplayer.view.JcPlayerView;
 import com.example.salt.Adapter.JcSongsAdapter;
 import com.example.salt.Model.GetSongs;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -27,6 +30,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class likedSongs_pg extends AppCompatActivity {
+
+    private Animation rotateOpen;
+    private Animation rotateClose;
+    private Animation fromBottom;
+    private Animation toBottom;
+    private boolean clicked = false;
+
+    FloatingActionButton menu_btn, recording_btn, playlist_btn;
 
     RecyclerView recyclerView;
     ProgressBar progressBar;
@@ -43,6 +54,39 @@ public class likedSongs_pg extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liked_songs_pg);
+
+        //FAB
+        rotateOpen = AnimationUtils.loadAnimation(this, R.anim.rotate_open_anim);
+        rotateClose = AnimationUtils.loadAnimation(this, R.anim.rotate_close_anim);
+        fromBottom = AnimationUtils.loadAnimation(this, R.anim.from_bottom_anim);
+        toBottom = AnimationUtils.loadAnimation(this, R.anim.to_bottom_anim);
+
+        menu_btn = findViewById(R.id.menu_btn);
+        menu_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onAddButtonClicked();
+            }
+        });
+        recording_btn = findViewById(R.id.recording_btn);
+        recording_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Your Recordings!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), RecordActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        playlist_btn = findViewById(R.id.playlist_btn);
+        playlist_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Your Playlist", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(likedSongs_pg.this, UploadAlbumActivity.class);
+                startActivity(intent);
+            }
+        });
 
         //Back Button.
         ImageView back_btn;
@@ -67,7 +111,7 @@ public class likedSongs_pg extends AppCompatActivity {
                 OneSongFragment fragment = new OneSongFragment(position);
                 fragment.setSong(songs);
                 FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-                ft.add(R.id.songFrag,fragment);
+                ft.add(R.id.songFrag, fragment);
                 ft.commit();
 //                Intent intent = new Intent(AllSongsActivity.this,OneSongActivity.class);
 //                intent.putExtra("song", songs);
@@ -82,20 +126,20 @@ public class likedSongs_pg extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 mupload.clear();
-                for(DataSnapshot das: snapshot.getChildren()){
+                for (DataSnapshot das : snapshot.getChildren()) {
                     GetSongs getSongs = das.getValue(GetSongs.class);
                     getSongs.setmKey(das.getKey());
                     currentIndex = 0;
                     mupload.add(getSongs);
                     checkin = true;
-                    jcAudios.add(JcAudio.createFromURL(getSongs.getSongTitle(),getSongs.getSongLink()));
+                    jcAudios.add(JcAudio.createFromURL(getSongs.getSongTitle(), getSongs.getSongLink()));
                 }
                 adapter.setSelectedPosition(0);
                 recyclerView.setAdapter(adapter);
                 adapter.notifyDataSetChanged();
                 progressBar.setVisibility(View.GONE);
 
-                if(checkin){
+                if (checkin) {
 
                 } else {
                     Toast.makeText(likedSongs_pg.this, "there is no songs!", Toast.LENGTH_SHORT).show();
@@ -109,14 +153,42 @@ public class likedSongs_pg extends AppCompatActivity {
         });
     }
 
-    public void changeSelectedSong(int index){
+    //FAB
+    private void onAddButtonClicked() {
+        setVisibility(clicked);
+        setAnimation(clicked);
+        clicked = !clicked;
+    }
+
+    private void setVisibility(boolean clicked) {
+        if (!clicked) {
+            recording_btn.setVisibility(View.VISIBLE);
+            playlist_btn.setVisibility(View.VISIBLE);
+        } else {
+            recording_btn.setVisibility(View.INVISIBLE);
+            playlist_btn.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void setAnimation(boolean clicked) {
+        if (!clicked) {
+            recording_btn.startAnimation(fromBottom);
+            playlist_btn.startAnimation(fromBottom);
+            menu_btn.startAnimation(rotateOpen);
+        } else {
+            recording_btn.startAnimation(toBottom);
+            playlist_btn.startAnimation(toBottom);
+            menu_btn.startAnimation(rotateClose);
+        }
+    }
+        public void changeSelectedSong ( int index){
         adapter.notifyItemChanged(adapter.getSelectedPosition());
         currentIndex = index;
         adapter.setSelectedPosition(currentIndex);
         adapter.notifyItemChanged(currentIndex);
     }
 
-    private void intiViews() {
+        private void intiViews () {
         recyclerView = findViewById(R.id.recyclerView);
         progressBar = findViewById(R.id.progressSong);
         recyclerView.setHasFixedSize(true);
@@ -125,14 +197,14 @@ public class likedSongs_pg extends AppCompatActivity {
         mupload = new ArrayList<>();
     }
 
-    @Override
-    public void onBackPressed() {
+        @Override
+        public void onBackPressed () {
         Intent intent = new Intent(getApplicationContext(), bolt_pg.class);
         startActivity(intent);
         finish();
     }
 
-    public ArrayList<JcAudio> getJcAudios() {
+        public ArrayList<JcAudio> getJcAudios () {
         return jcAudios;
     }
-}
+    }
